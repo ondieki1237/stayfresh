76db06273d1935f737c761995d3bb2d93bba27ae
seth <seth@seth.seth>
2025-11-08 07:52:40 +0300

    ready

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL || "https://www.kisumu.codewithseth.co.ke/api"
